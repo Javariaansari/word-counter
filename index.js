2053e7ba95fcbc91c690a035dfbae41faf8a481a
Javariaansari <javariaansari01@gmail.com>
2024-04-23 07:01:52 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+// Import athe 'inquirer' module, which is a commond line interface for node.js
+import inquirer from "inquirer";
+// Declear a constant 'answers' and assign it to the result of inqurier.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the word:"
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+// print the array of words to the console
+console.log(words);
+// print the words count of the sentence to the console
+console.log(`your sentence word count is ${words.length}`);
